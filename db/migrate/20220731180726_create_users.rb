232f4c8c38b9e2d400758c0043c56bb09a153583
Troy Coll <troy.coll@gmail.com>
2022-07-31 14:08:24 -0400

    user model

---
+++ db/migrate/20220731180726_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users, id: :uuid do |t|
+      t.uuid :id
+      t.string :name
+      t.string :email
+      t.string :salt
+      t.string :passhash
+
+      t.timestamps
+    end
+  end
+end
