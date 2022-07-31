3b04f78f8645ae00988c95de619f89d7949f64c5
Troy Coll <troy.coll@gmail.com>
2022-07-31 14:35:22 -0400

    seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_31_175252) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_31_180726) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -50,6 +50,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_31_175252) do
   create_table "stockrooms", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "salt"
+    t.string "passhash"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,62 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'csv'
+
+category_file_path = 'db/data/categories.csv'
+puts "seeding from #{category_file_path}"
+
+category_csv = CSV.read(category_file_path, headers: true)
+
+category_csv.each do |row|
+  data = {
+    name: row[0],
+    description: row[1]
+  }
+  Category.create(data)
+end
+
+item_file_path = 'db/data/items.csv'
+puts "seeding from #{item_file_path}"
+
+item_csv = CSV.read(item_file_path, headers: true)
+
+item_csv.each do |row|
+  data = {
+    name: row[0],
+    category: row[1],
+    description: row[2],
+    avg_price: row[3],
+    stock_level: row[4],
+    exp_date: row[5]
+  }
+  Item.create(data)
+end
+
+stockroom_file_path = 'db/data/stockrooms.csv'
+puts "seeding from #{stockroom_file_path}"
+
+stockroom_csv = CSV.read(stockroom_file_path, headers: true)
+
+stockroom_csv.each do |row|
+  data = {
+    name: row[0],
+    description: row[1]
+  }
+  Stockroom.create(data)
+end
+
+vendor_file_path = 'db/data/vendors.csv'
+puts "seeding from #{vendor_file_path}"
+
+vendor_csv = CSV.read(vendor_file_path, headers: true)
+
+vendor_csv.each do |row|
+  data = {
+    name: row[0],
+    website: row[1],
+    location: row[2],
+    country: row[3]
+  }
+  Vendor.create(data)
+end
